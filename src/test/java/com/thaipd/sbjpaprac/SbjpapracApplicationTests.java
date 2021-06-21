a539fa652bfe49d571dbaf4c8936c56b7cb2e8d2
thaipd <thaipd@vivas.vn>
2021-06-21 10:10:07 +0700

    start project

---
+++ src/test/java/com/thaipd/sbjpaprac/SbjpapracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thaipd.sbjpaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbjpapracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
