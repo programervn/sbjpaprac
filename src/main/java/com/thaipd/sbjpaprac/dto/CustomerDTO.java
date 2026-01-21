84bba10b0d74e6e52558bdcfffbeeb2c5baf2490
Programervn <phanthai@gmail.com>
2026-01-21 13:48:03 +0700

    update trail splash

--- src/main/java/com/thaipd/sbjpaprac/dto/CustomerDTO.java
+++ src/main/java/com/thaipd/sbjpaprac/dto/CustomerDTO.java
@@ -1,13 +1,9 @@
 package com.thaipd.sbjpaprac.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
 public record CustomerDTO(
-        Long customerId,
-        String name,
-        String address,
-        String website,
-        Long creditLimit
-) {}
+                Long customerId,
+                String name,
+                String address,
+                String website,
+                Long creditLimit) {
+}
